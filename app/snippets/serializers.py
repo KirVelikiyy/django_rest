48f2b7d0a5abfa0c44513c43aec17086132afea6
KirVelikiy <lisov.kirill50@gmail.com>
2023-06-16 17:07:03 +0400

    Delete unnecessary fields of SnippetSerializer

--- app/snippets/serializers.py
+++ app/snippets/serializers.py
@@ -1,16 +1,9 @@
 from rest_framework import serializers
 
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from .models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=100, required=False, allow_blank=True)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
