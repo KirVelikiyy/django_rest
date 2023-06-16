887060a03c88c20d2e787c9e516eb4b442f4d570
KirVelikiy <lisov.kirill50@gmail.com>
2023-06-16 17:49:06 +0400

    Add views for snippets app

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+
 from quickstart import views
 
 router = routers.DefaultRouter()
@@ -25,6 +26,7 @@ router.register(r'groups', views.GropViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
