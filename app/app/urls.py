1ed7ae1d6f67feee703e4bb895dc99a3b4727c9e
KirVelikiy <lisov.kirill50@gmail.com>
2023-06-19 12:21:52 +0400

    Add authentication and permissions

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,16 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 
-from quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GropViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
